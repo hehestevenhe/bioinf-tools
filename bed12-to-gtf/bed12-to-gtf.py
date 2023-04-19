d4544c3d6958f94ca96a99c8cb3b6f803224ce69
hehestevenhe <115518768+hehestevenhe@users.noreply.github.com>
2023-04-19 10:55:23 +1000

    Update bed12-to-gtf.py
    The manual input tool has been depreciated and will be superseded by CLI versions only. The commits updates the bed-to-gtf file by correctly converting to gtf format (was originally gff, which will be included as a new appropriately named file). The gtf conversion no longer requires a chromosome size tsv input. Intends for use with bed inputs generated by TRAWLING and parses the bed information to include:
    - transcript, exon, CDS and start/stop codon features
    - gene_id, gene_name, transcript_id and transcript_biotype as additional attributes (Column 9 of generated gtf files)

--- bed12-to-gtf/bed12-to-gtf.py
+++ bed12-to-gtf/bed12-to-gtf.py
@@ -1,51 +1,54 @@
-### Code from https://bioinformatics.stackexchange.com/questions/2242/how-to-convert-bed-to-gff3
-#The .tsv contians chromosome names and lengths in two columns
+### Code adapted from https://bioinformatics.stackexchange.com/questions/2242/how-to-convert-bed-to-gff3
+### The intended bed file inputs originally come from gtf files which have been converted using bedparse::gtf2bed and processed in TRAWLING
+### This gtf2bed method only parses transcript, exon and CDS/start_codon/stop_codon information from the gtf file; all other features are lost (eg. UTRs)
+### As a result, the output from this script only contains transcript, exon and CDS features (start and stop codon info is unretrievable from the gtf2bed conversion)
+### Code for this initial conversion which was helpful for reversing the conversion can be found at https://github.com/tleonardi/bedparse/blob/master/bedparse/converters.py
 import os
 import os.path
-def program():
-   chrom_tsv = input("Enter chromosome length tsv file: ")
-   bed12 = input("Enter bed12 file: ")
-   output = input("Please name the output .gtf file: ")
-   while True:
-      print("Please confirm the above inputs are correct (y/n)")
-      confirm = input()
-      if confirm in ("y","n"):
-         break
-      print("Invalid input: Please enter 'y' or 'n' ")
-            
-   if confirm == "y":
-      a = os.path.isfile(chrom_tsv)
-      b = os.path.isfile(bed12)
-      if a and b == True:
-         print("Processing...")
-         ### This is the code that does the actual processing of the .bed into .gtf
-         with open(output+".gtf", "a") as f:  
-            for line in open(chrom_tsv):
-               fields = line.strip().split("\t")
-               print(fields[0], ".", "contig","1",str(fields[1]), ".", "+", ".", "ID=%s" % fields[0], file = f, sep= '\t')
-         
-            for line in open(bed12):
-               fields = line.strip().split("\t")
-               contig = fields[0]
-               # note: BED is 0-based, half-open, GFF is 1-based, closed
-               start= (int(fields[1]) + 1)
-               end=fields[2]
-               name=fields[3]
-               score=fields[4]
-               strand=fields[5]
-               print(contig, "from_file:" + bed12, "element", str(start), end, score, strand, ".", "ID=%s;parent=%s" % (name, contig), sep='\t', file=f)
+import argparse
 
-               block_sizes = map(int, fields[10][:-1].split(","))   # The [:-1] trims the last element from the field which is a "," If this is not trimmed an additional '' value exists in the list
-               block_starts = map(int,fields[11][:-1].split(","))    # which int() cannot act on, resulting in a traceback
-            
-               for (block, (bstart, blen)) in enumerate(zip(block_starts, block_sizes)):
-                  bend = start - 1 + bstart + blen   # without the -1 the bend extends 1 position after the element's end
-                  print(contig, "from_file:" + bed12, "sub-element", str(start + bstart), str(bend), score, strand, ".", "ID=%s_%i;parent=%s" %(name, block, name), sep='\t', file = f)
-         print("GTF file " + output + ".gtf completed!")
-         ###
-      else:
-         print("Input files cannot be found -- include file paths if these are not in the same directory as this .py file\nExiting program")
-         exit()
+parser = argparse.ArgumentParser()
+parser.add_argument("-b", "--bed", help = "specify a bed file to use as input (bed12 format)")
+parser.add_argument("-o", "--output", help = "specify name of the .gtf output file ")
+args = parser.parse_args()
+
+try:
+   args.bed
+   args.output
+   a = os.path.isfile(args.bed)
+   if a == True :
+      ### This is the code that does the actual processing of the .bed into .gtf
+      print("Processing...")
+      with open(args.output, "a") as f:  
+         for line in open(args.bed):
+            fields = line.strip().split("\t")
+            contig = fields[0]
+            # note: BED is 0-based, half-open, GTF is 1-based, closed
+            # GTF CDS annotation include the start but NOT the stop codon (https://github.com/NBISweden/GAAS/blob/master/annotation/knowledge/gxf.md)
+            start=(int(fields[1]) + 1)
+            end=fields[2]
+            name=fields[3]
+            score=fields[4]
+            strand=fields[5]
+            cds_start=(int(fields[6]) + 1)
+            cds_end=(int(fields[7]) - 3)
+            gene_name=fields[12]    ### These are extraFields from gtf2bed which have no strict convetion in bed 
+            gene_id=fields[13]      ### make sure they are present and column numbers are correct!
+            transcript_biotype=fields[14]
+            print(contig, "from_file:" + args.bed, "transcript", str(start), end, score, strand, ".", 'gene_id "%s"; gene_name "%s"; transcript_id "%s"; transcript_biotype "%s";' % (gene_id, gene_name, name, transcript_biotype), sep='\t', file=f)
+            if fields[6]!=fields[7]:
+               print(contig, "from_file:" + args.bed, "CDS", str(cds_start), cds_end, score, strand, ".", 'gene_id "%s"; gene_name "%s"; transcript_id "%s"; transcript_biotype "%s";' % (gene_id, gene_name, name, transcript_biotype), sep='\t', file=f)
+               print(contig, "from_file:" + args.bed, "start_codon", str(cds_start), (cds_start + 2), score, strand, ".", 'gene_id "%s"; gene_name "%s"; transcript_id "%s"; transcript_biotype "%s";' % (gene_id, gene_name, name, transcript_biotype), sep='\t', file=f)
+               print(contig, "from_file:" + args.bed, "stop_codon", (cds_end + 1), (cds_end + 3), score, strand, ".", 'gene_id "%s"; gene_name "%s"; transcript_id "%s"; transcript_biotype "%s";' % (gene_id, gene_name, name, transcript_biotype), sep='\t', file=f)
+            block_sizes = map(int, fields[10][:-1].split(","))    # The [:-1] trims the last element from the field which is a "," If this is not trimmed an additional '' value exists in the list
+            block_starts = map(int,fields[11][:-1].split(","))    # which int() cannot act on, resulting in a traceback
+         
+            for (block, (bstart, blen)) in enumerate(zip(block_starts, block_sizes)):
+               bend = start - 1 + bstart + blen   # without the -1 the bend extends 1 position after the element's end
+               print(contig, "from_file:" + args.bed, "exon", str(start + bstart), str(bend), score, strand, ".", 'gene_id "%s"; gene_name "%s"; ID "%s_%i"; transcript_id "%s"; transcript_biotype "%s";' %(gene_id, gene_name, name, block, name, transcript_biotype), sep='\t', file = f)
+      print("GTF file " + args.output +" completed!")
+      ###
    else:
-      program()
-program()
+      print("Invalid inputs; check paths and ensure parameters are given for --bed and --output")
+except:
+   print("Invalid inputs; check paths and ensure parameters are given for --bed and --output")
